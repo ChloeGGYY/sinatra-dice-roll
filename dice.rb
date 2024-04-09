31987a8d32c1cafbfedd96052c8ed25a545ff7b8
ChloeGGYY <97067927+ChloeGGYY@users.noreply.github.com>
2024-04-09 05:23:30 +0000

    Dice Roll_Further

--- dice.rb
+++ dice.rb
@@ -1,8 +1,15 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+  
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "Hello World"
+  erb(:elephant)
 end
 
 get("/zebra") do
@@ -15,56 +22,54 @@ end
 
 get("/dice/2/6") do
   first_die = rand(1..6)
-  second_die = rand(1..6)
+	second_die = rand(1..6)
   sum = first_die + second_die
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+	erb(:two_six)
 end
 
 get("/dice/2/10") do
   first_die = rand(1..10)
-  second_die = rand(1..10)
+	second_die = rand(1..10)
   sum = first_die + second_die
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  "<h1>2d6</h1>
+	"<h1>2d10</h1>
    <p>#{outcome}</p>"
 end
 
 get("/dice/1/20") do
-  first_die = rand(1..20)
-	
-  outcome = "You rolled a #{first_die}."
-	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  @die = rand(1..20)
+
+  @outcome = "You rolled a #{@die}."
+
+  erb(:one_twenty)
 end
 
 get("/dice/5/4") do
   first_die = rand(1..4)
-  second_die = rand(1..4)
+	second_die = rand(1..4)
   third_die = rand(1..4)
   fourth_die = rand(1..4)
   sum = first_die + second_die + third_die + fourth_die
 	
-  outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die}, for a total of #{sum}."
+	outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
 	
-  "<h1>2d6</h1>
+	"<h1>5d4</h1>
    <p>#{outcome}</p>"
 end
 
-get("/homepage") do
-  <<-HTML
-  <h1>Dice Roll</h1>
-  <p>
-  <a href='/dice/2/6'>Roll two 6-sided dice</a><br>
-  <a href='/dice/2/10'>Roll two 10-sided dice</a><br>
-  <a href='/dice/1/20'>Roll one 20-sided dice</a><br>
-  <a href='/dice/5/4'>Roll five 4-sided dice</a><br>
-  </p>
-  HTML
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
